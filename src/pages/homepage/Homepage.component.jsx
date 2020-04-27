e613f7ed7398c94b42a854c0815c9d46e6aa2dc5
vinay <vinaykr4321@gmail.com>
2020-04-27 12:41:24 +0530

    Finished styling of Menu-item and break Homepage into small components

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/directory/Directory.component'
+
+import './Homepage.styles.scss';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+
+export default Homepage;
