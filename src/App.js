c408c1cfe34e5a295cbaa45518dca5db8c741930
vinay <vinaykr4321@gmail.com>
2020-05-02 12:13:03 +0530

    Updating our app with collection-preview and collection-item component

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Homepage from './pages/homepage/Homepage.component';
+import ShopPage from './pages/shop/shop.component' ;
 
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// )
 
-const HatsPages = (props) => (
-  <div>
-    <h1>MULTI HAT PAGES WITH ID: {props.match.params.hatid}</h1>
-  </div>
-)
+// const HatsPages = (props) => (
+//   <div>
+//     <h1>MULTI HAT PAGES WITH ID: {props.match.params.hatid}</h1>
+//   </div>
+// )
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
-        <Route exact path='/hats/:hatid' component={HatsPages} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
